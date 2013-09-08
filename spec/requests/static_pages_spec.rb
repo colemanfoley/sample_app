fac2fd62f6e7bd949d6c374beda46a6acd1f72b3
Coleman Foley <colemanfoley@gmail.com>
2013-09-08 14:39:56 -0700

    Added about page and corresponding test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,4 +16,12 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
   end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
